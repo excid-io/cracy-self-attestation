f7c9239a474d7b961102bb410aaebe98d7880303
Fotios Bistas <fotis.bistas@gmail.com>
2025-11-27 07:11:13 +0200

    Add all question sets
    Signed-off-by: Fotios Bistas <fotis.bistas@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,37 +1,77 @@
 export const QUESTION_SETS = [
-  {
-    id: "technical-documentation",
-    name: "Technical Documentation – Annex VII",
-    file: "./questions/machine-readable/technical_documentation_annex_6.json"
-  },
-  {
-    id: "reporting-obligations",
-    name: "Reporting Obligations of Manufacturerers – Article 14",
-    file: "./questions/machine-readable/reporting_obligations_of_manufacturers_article_14.json"
-  },
-  //{
-  //  id: "conformity-assessment-procedures",
-  //  name: "Conformity Assessment Procedures – Annex VIII",
-  //  file: "./questions/conformity_assessment_procedures_annex_7.md"
-  //},
-  //{
-  //  id: "eu-declaration-of-conformity",
-  //  name: "EU Declaration of Conformity – Annex V",
-  //  file: "./questions/eu_declaration_of_conformity_annex_5.md"
-  //},
-  //{
-  //  id: "essential-cybersecurity-requirements",
-  //  name: "Essential Cybersecurity Requirements – Annex I",
-  //  file: "./questions/essential_cybersecurity_requirements_annex_1.md"
-  //},
-  //{
-  //  id: "presumption-of-conformity",
-  //  name: "Presumption of Conformity – Article 27",
-  //  file: "./questions/presumption_of_conformity_article_27.md"
-  //},
-  //{
-  //  id: "instructions-to-the-user",
-  //  name: "Instructions to the User – Annex II",
-  //  file: "./questions/instructions_to_the_user_annex_2.md"
-  //},
+    {
+        id: "conformity-assessment-procedures-annex-7",
+        name: "Conformity Assessment Procedures – Annex VII",
+        file: "./questions/machine-readable/conformity_assessment_procedures_annex_7.json"
+    },
+    {
+        id: "design-development-cybersecurity-risk-management-article-13",
+        name: "Design & Development Cybersecurity Risk Management – Article 13",
+        file: "./questions/machine-readable/design_development_cybersecurity_risk_management_article_13.json"
+    },
+    {
+        id: "essential-cybersecurity-requirements-annex-1",
+        name: "Essential Cybersecurity Requirements – Annex I",
+        file: "./questions/machine-readable/essential_cybersecurity_requirements_annex_1.json"
+    },
+    {
+        id: "eu-declaration-of-conformity-annex-5",
+        name: "EU Declaration of Conformity – Annex V",
+        file: "./questions/machine-readable/eu_declaration_of_conformity_annex_5.json"
+    },
+    {
+        id: "eu-declaration-of-conformity-article-28",
+        name: "EU Declaration of Conformity – Article 28",
+        file: "./questions/machine-readable/eu_declaration_of_conformity_article_28.json"
+    },
+    {
+        id: "identification-of-economic-operators-article-23",
+        name: "Identification of Economic Operators – Article 23",
+        file: "./questions/machine-readable/identification_of_economic_operators_article_23.json"
+    },
+    {
+        id: "instructions-to-the-user-annex-2",
+        name: "Instructions to the User – Annex II",
+        file: "./questions/machine-readable/instructions_to_the_user_annex_2.json"
+    },
+    {
+        id: "other-provisions-related-to-reporting-article-17",
+        name: "Other Provisions Related to Reporting – Article 17",
+        file: "./questions/machine-readable/other_provisions_related_to_reporting_article_17.json"
+    },
+    {
+        id: "presumption-of-conformity-article-27",
+        name: "Presumption of Conformity – Article 27",
+        file: "./questions/machine-readable/presumption_of_conformity_article_27.json"
+    },
+    {
+        id: "reporting-obligations-of-manufacturers-article-14",
+        name: "Reporting Obligations of Manufacturers – Article 14",
+        file: "./questions/machine-readable/reporting_obligations_of_manufacturers_article_14.json"
+    },
+    {
+        id: "rules-and-conditions-for-affixing-the-ce-article-30",
+        name: "Rules & Conditions for Affixing the CE Marking – Article 30",
+        file: "./questions/machine-readable/rules_and_conditions_for_affixing_the_ce_article_30.json"
+    },
+    {
+        id: "single-reporting-platform-article-16",
+        name: "Single Reporting Platform – Article 16",
+        file: "./questions/machine-readable/single_reporting_platform_article_16.json"
+    },
+    {
+        id: "technical-documentation-annex-6",
+        name: "Technical Documentation – Annex VI",
+        file: "./questions/machine-readable/technical_documentation_annex_6.json"
+    },
+    {
+        id: "technical-documentation-article-31",
+        name: "Technical Documentation – Article 31",
+        file: "./questions/machine-readable/technical_documentation_article_31.json"
+    },
+    {
+        id: "voluntary-reporting-article-15",
+        name: "Voluntary Reporting – Article 15",
+        file: "./questions/machine-readable/voluntary_reporting_article_15.json"
+    }
 ];
